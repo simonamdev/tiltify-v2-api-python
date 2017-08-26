d004cb110183e78913e5520a61359b386340ece7
Simon AM <simon@agius-muscat.net>
2017-08-26 16:02:22 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='tiltify2',
+    version='1.0',
+    packages=['tiltify2'],
+    url='https://github.com/purrcat259/tiltify-v2-api-python',
+    license='MIT',
+    author='Simon Agius Muscat',
+    author_email='simon@agius-muscat.net',
+    description='A wrapper for the 2nd version of the Tiltify API',
+    keywords=['tiltify', 'api', 'v2', 'livestreaming', 'charity', 'fundraising']
+)
