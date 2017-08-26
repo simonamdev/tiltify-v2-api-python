594474dfec9b9613e736e8fdc8418e40c729e139
Simon AM <simon@agius-muscat.net>
2017-08-26 09:23:48 +0200

    Added exception for not passing a string as the API key

--- tests/test_tiltify2.py
+++ tests/test_tiltify2.py
@@ -4,7 +4,11 @@ from tiltify2.tiltify import Tiltify2
 
 
 class TestTiltify2:
-    def test_not_passing_valid_api_key_throws_exception(self):
+    @pytest.mark.parametrize('api_key', [
+        '',
+        None
+    ])
+    def test_not_passing_valid_api_key_throws_exception(self, api_key):
         with pytest.raises(ValueError):
-            tiltify = Tiltify2('')
+            tiltify = Tiltify2(api_key)
 
--- tiltify2/tiltify.py
+++ tiltify2/tiltify.py
@@ -4,5 +4,7 @@ class Tiltify2:
         self.__validate_api_key()
 
     def __validate_api_key(self):
+        if not isinstance(self._api_key, str):
+            raise ValueError('API Key must be a string')
         if not len(self._api_key) == 32:
             raise ValueError('Invalid API Key length of {}. API key must be 32 characters long'.format(len(self._api_key)))
